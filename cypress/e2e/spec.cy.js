1af6697a99fb0c6ef9b9f751186ebe8174ed7201
ChuchoMC <chuchomc58@gmail.com>
2024-07-06 12:14:45 -0700

    2nd test added

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,3 +5,13 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Pokedex', function() {
+  it('Lnk clicked', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click();
+    cy.contains('chlorophyll')
+  
+  })
+})
+
