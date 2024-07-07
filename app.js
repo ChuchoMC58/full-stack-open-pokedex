1f098747db7a67206d7db2c6f72637ef92336c9e
ChuchoMC <chuchomc58@gmail.com>
2024-07-07 14:21:39 -0700

    Deploying with errors..

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+// app.get('/version', (req, res) => {
+//   res.send('2') // change this string to ensure a new version deployed
+// })
+
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
 })
 
-app.listen(PORT, '0.0.0.0', () => {
-  this_causes_error
-  // eslint-disable-next-line no-console
+app.listen(PORT , '0.0.0.0', () => {
   console.log(`server started on port http://0.0.0.0:${PORT}`)
 })
