50a98a7125537a7142f1c56e22f9f041cf868418
ChuchoMC <chuchomc58@gmail.com>
2024-07-10 19:43:24 -0700

    arreglando elints

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,17 +1,23 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
+    /* eslint-disable no-undef */
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    /* eslint-enable no-undef */
+
   })
 })
 
 describe('Pokedex', function() {
   it('Lnk clicked', function() {
+    /* eslint-disable no-undef */
     cy.visit('http://localhost:5000')
-    cy.contains('ivysaur').click();
+    cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
-  
+    /* eslint-enable no-undef */
+
+
   })
 })
 
